d2746cfeecd980129c378857421c73f074d83ff1
Fouad Wahabi <fouad.wahabi@gmail.com>
2017-04-02 14:15:11 +0100

    Fix cors

--- config/lib/express.js
+++ config/lib/express.js
@@ -105,12 +105,7 @@ module.exports.initMiddleware = function (app) {
   }));
 
   // Add cors middleware
-  app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
-    next();
-  });
+  app.use(cors())
 
 };
 
