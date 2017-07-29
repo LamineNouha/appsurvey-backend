6a804bbb3261c6367bf3d76cfe2fbb59e36088e9
Nouha <lamine.nooha@gmail.com>
2017-07-29 15:48:12 +0100

    first commit

--- modules_src/users/server/routes/users.server.routes.js
+++ modules_src/users/server/routes/users.server.routes.js
@@ -10,8 +10,8 @@ module.exports = function (app) {
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
   app.route('/api/users/password').post(users.changePassword);
   app.route('/api/users/picture').post(users.changeProfilePicture);
-  app.route('/api/users/events').post(users.interestEvent);
-  app.route('/api/users/organizations').post(users.followOrganization);
+ 
+ 
 
   // Finish by binding the user middleware
   app.param('userId', users.userByID);
